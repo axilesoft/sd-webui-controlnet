be3bc798089f42e80f6eedd7dbcd263223338eb9
axilesoft <axile.app.support@outlook.com>
2023-02-24 12:56:20 +0800

    Revert "Depth mode input depth map directly"
    This reverts commit 3b61d380fd245497cc0f91cae68e8639838aad9b.

--- scripts/controlnet.py
+++ scripts/controlnet.py
@@ -141,7 +141,6 @@ class Script(scripts.Script):
             "scribble": simple_scribble,
             "fake_scribble": fake_scribble,
             "segmentation": uniformer,
-            "depth (input depth map)": midasDepthMap,
         }
         self.unloadable = {
             "hed": unload_hed,
